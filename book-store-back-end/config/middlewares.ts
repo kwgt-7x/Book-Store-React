eb90f8331decfacda8356a7bf592599a527840da
kwgt-7x <kadwgt7@gmail.com>
2026-07-19 22:59:58 +0300

    fix: update strapi cors origins for vercel

--- book-store-back-end/config/middlewares.ts
+++ book-store-back-end/config/middlewares.ts
@@ -4,7 +4,18 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'https://book-store-react-sandy.vercel.app'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
